709ae0974570cefadc3a913b27cd0fc51028c508
devinlukianto <devindevdog@gmail.com>
2017-05-30 10:27:53 +0700

    init home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+	<head>
+		<meta charset="utf-8">
+		<title>JC Salon - Home</title>
+		<link href="{{URL::asset('css/style.css')}}" rel="stylesheet">
+
+	</head>
+	<body>
+		<p>WELCOME TO OUR SALON</p>
+	</body>
+</html>
